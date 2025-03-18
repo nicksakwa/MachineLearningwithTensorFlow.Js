4882ffb51eb122483f2fdc6d5a501aba1c0e68f8
nsakwa <nsakwa@gmail.com>
2025-03-19 01:17:44 +0300

    fetchdata-initial

--- fetchdata.js
+++ fetchdata.js
@@ -1,14 +1,27 @@
-function extractData(obj) {
-    return { x: obj.Horsepower, y: obj.Miles_per_Gallon };
-  }
-  
-  let carData = [
-    { Make: "Toyota", Model: "Camry", Horsepower: 150, Miles_per_Gallon: 30, Color: "Silver" },
-    { Make: "Honda", Model: "Civic", Horsepower: 120, Miles_per_Gallon: 35, Color: "Blue" },
-    { Make: "Ford", Model: "Mustang", Horsepower: 300, Miles_per_Gallon: 20, Color: "Red" },
-    { Make: "Chevrolet", Model: "Impala", Horsepower: null, Miles_per_Gallon: 25, Color: "Black" }
-  ];
-  
-  let extractedCarData = carData.map(extractData);
-  
-  console.log(extractedCarData);
+const fs=require('fs');
+const path =require('path');
+function extractData(obj){
+    return {x: obj.Horsepower, y: obj.Miles_per_Gallon };
+}
+ function removeErrors(obj){
+    return obj.x != null && obj.y != null;
+ }
+
+ async function runTF(){
+    try {
+    const fetch = await import('node-fetch').then(module => module.default);
+    const filePath = path.join('file://',  __dirname,  'cardata.json');
+    const jsonData = await fetch(filePath);
+    if (!jsonData.ok){
+        throw new Error('HTTP error! status: ${jsonData.status}');
+    let values = await jsonData.json();
+    values = values.map(extractData).filter(removeErrors);
+    console.log(values);
+    }
+    }
+    catch (error){
+        console.error("Error", error);
+    }
+}
+ 
+ runTF();
